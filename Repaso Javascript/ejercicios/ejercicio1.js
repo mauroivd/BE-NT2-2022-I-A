ed6d52232260380045e1735855f8a9aad5f583ec
FdValls <fernandodanielvalls@gmail.com>
2022-04-08 13:53:47 -0300

    test

--- Repaso Javascript/ejercicios/ejercicio1.js
+++ Repaso Javascript/ejercicios/ejercicio1.js
@@ -1,76 +1,63 @@
 /**
- * Hacer una funcion que dado un array de participantes del curso me devuelva el alumno con mayor edad.
- * 
- * [
- *  {
- *    firstName,
- *    lastName,
- *    age
- *  },
- *  ...,
- *  n
- * ]
+ * CONSIGNA: Realizar una funcion que dado un array de objetos `personas` 
+ * devuelva un array con un atributo `allowed` (_boolean_) que indique si puede
+ *  entrar o no en un casino (Solo los mayores de 21 anios pueden entrar)
  */
 
+const LIMITE_EDAD = 21
 
-function participanteMayorEdad(arr){
-
-  console.log(arr.length)
-
-  arr.forEach((elem, index) => {
-    console.log(`Arr[${index}]`, elem)
-
-
-  })
-  
-  console.log('************** OTRA FORMA DE RECORRER ARRAYS')
-  for (var i = 0; i < arr.length; i++){
-    console.log(`Arr[${i}]`, arr[i])
-  }
-
-}
-
-
-
-const alumnos =[
+ const array = [
   {
-    firstName: "Daniel",
-    lastName: "Guzman",
-    age: 23
+    nombre: "Alejandra",
+    apellido: "Perez",
+    edad: 25
   },
   {
-    firstName: "Kenaan",
-    lastName: "Chab",
-    age: 25
+    nombre: "Jezza",
+    apellido: "Gonzales",
+    edad: 19
   },
   {
-    firstName: "Mauricio",
-    lastName: "Villaroel",
-    age: 20
+    nombre: "Eduardo",
+    apellido: "Perez",
+    edad: 25
   }
 ]
 
-console.log(alumnos)
+const verificarArray = (item) => {
+  if(item.edad >= LIMITE_EDAD)
+    item.allowed = true
+  else
+    item.allowed = false
+  return item
+}
 
-participanteMayorEdad(alumnos)
+const array_resultante = array.map(verificarArray)
+// console.log(array_resultante[0], array_resultante[1], array_resultante[2])
+
+console.log(typeof verificarArray)
+
+const otro_array = array.map(item => item.edad >= LIMITE_EDAD? {...item, allowed: true}: item)
+
+// console.log(otro_array)
+
+const myFilter = array_resultante.filter(item => item.allowed)
+
+console.log(myFilter)
+
+
+
 
 /**
-const miFuncion = function(){
-  console.log("hola mundo")
-}
-
-function saludo(){
-  console.log("Hola mundo")
-}
-
-const otroSaludo = () => {
-  console.log("Hola mundo")
-}
-
-const otroSaludoMas = () => console.log("Hola mundo")
-
-miFuncion()
-saludo()
-otroSaludoMas()
-
+ * Destructuring Objects
  */
+
+//  const [persona0, persona1] = array_resultante
+
+//  console.log(persona0)
+ 
+//  const { edad, nombre, allowed } = persona0
+ 
+//  console.log(`Mi nueva constante (destructurada) edad: ${edad}`)
+ 
+//  console.log(nombre, edad, allowed)
